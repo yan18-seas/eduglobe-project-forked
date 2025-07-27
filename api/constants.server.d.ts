6a49efbaf54990d1ee86cf3ff2e0fb9befbcfa26
anika-c123 <a8chadha@gmail.com>
2025-07-27 17:17:51 -0400

    Update constants.server.d.ts

--- api/constants.server.d.ts
+++ api/constants.server.d.ts
@@ -1,13 +1,17 @@
-export enum Language {
-  ENGLISH = 'English',
-  TIBETAN = 'བོད་མི (Tibetan)',
-  HAWAIIAN = 'ʻŌlelo Hawaiʻi (Hawaiian)',
-  TELUGU = 'తెలుగు (Telugu)',
-}
-
-export const SYSTEM_PROMPTS: Record<Language, string>;
-
 export const Role: {
   USER: 'user';
   AI: 'ai';
 };
+
+export type Role = (typeof Role)[keyof typeof Role];
+
+export const Language: {
+  ENGLISH: 'English';
+  TIBETAN: 'བོད་མི (Tibetan)';
+  HAWAIIAN: 'ʻŌlelo Hawaiʻi (Hawaiian)';
+  TELUGU: 'తెలుగు (Telugu)';
+};
+
+export type Language = (typeof Language)[keyof typeof Language];
+
+export const SYSTEM_PROMPTS: Record<Language, string>;
