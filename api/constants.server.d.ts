c039708440e2a3bfa17aabcb17113d1337d30f38
anika-c123 <a8chadha@gmail.com>
2025-07-27 00:20:17 -0400

    Create constants.server.d.ts

---
+++ api/constants.server.d.ts
@@ -0,0 +1,8 @@
+export enum Language {
+  ENGLISH = 'English',
+  TIBETAN = 'བོད་མི (Tibetan)',
+  HAWAIIAN = 'ʻŌlelo Hawaiʻi (Hawaiian)',
+  TELUGU = 'తెలుగు (Telugu)',
+}
+
+export const SYSTEM_PROMPTS: Record<Language, string>;
