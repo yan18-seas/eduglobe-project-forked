764c5689f5714c2e8f4fee3a9484468ecc33cdad
anika-c123 <a8chadha@gmail.com>
2025-07-27 01:09:01 -0400

    Update constants.server.js

--- api/constants.server.js
+++ api/constants.server.js
@@ -1,14 +1,14 @@
 export const Role {
   USER = 'user',
   AI = 'ai',
-}
+};
 
 export const Language {
   ENGLISH = 'English',
   TIBETAN = 'བོད་མི (Tibetan)',
   HAWAIIAN = 'ʻŌlelo Hawaiʻi (Hawaiian)',
   TELUGU = 'తెలుగు (Telugu)',
-}
+};
 
 export const LANGUAGES = [
   Language.ENGLISH,
